eebef90956a6dc85e13052351766af79efc8046c
soyjefu <5ll34090@gmail.com>
2025-09-11 10:20:19 +0900

    modified:   .gitignore modified:   account-app/theprepared_ac/settings.py

--- account-app/theprepared_ac/settings.py
+++ account-app/theprepared_ac/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w!c5^)ts$jsp3%546gwi)a6kywnd=@sw3242ke-unfm)vk^nls'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['account.theprepared.kr', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://account.theprepared.kr']
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
